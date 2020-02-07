37b6948bf0bb4b33113d053ce7f7317aad08c506
dennisfrijlink <31823687+dennisfrijlink@users.noreply.github.com>
2020-02-07 13:41:35 +0100

    Integrate nuxt js (#15)
    * Made nuxt project ready for start developing
    
    * fix assets (main.css)
    
    * Add typescript support
    
    * remove 'test' file
    
    Co-authored-by: Bas <31634060+b9a9s9@users.noreply.github.com>

--- nuxt.config.js
+++ nuxt.config.js
@@ -23,6 +23,7 @@ export default {
   ** Global CSS
   */
   css: [
+    '~/assets/main.css'
   ],
   /*
   ** Plugins to load before mounting the App
@@ -33,6 +34,7 @@ export default {
   ** Nuxt.js dev-modules
   */
   buildModules: [
+    '@nuxt/typescript-build'
   ],
   /*
   ** Nuxt.js modules
