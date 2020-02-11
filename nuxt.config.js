5b6e38f07210cb18480a8563a5e2f82c92df8cd8
B9a9s9 <bas.vaneck@ziggo.nl>
2020-02-11 11:23:20 +0100

    initial placing of host/join buttons and logo (image still needs to be made)

--- nuxt.config.js
+++ nuxt.config.js
@@ -42,6 +42,7 @@ export default {
   modules: [
     // Doc: https://axios.nuxtjs.org/usage
     '@nuxtjs/axios',
+    'nuxt-buefy'
   ],
   /*
   ** Axios module configuration
--- pages/index.vue
+++ pages/index.vue
@@ -1,14 +1,33 @@
 <template>
-  <div></div>
+  <section>
+    <div style="text-align: center">
+    <img src="https://cdn.discordapp.com/attachments/648842340273750026/675728343584342039/Structure.png" alt="Logo">
+    </div>
+
+    <div class="has-text-centered" style="margin: 100px">
+      <b-button class="is-rounded is-large" icon-left="" tag="nuxt-link" to="/">Host</b-button>
+      <b-button class="is-rounded is-large" icon-left="" tag="nuxt-link" to="/">Join</b-button>
+    </div>
+  </section>
 </template>
 
 <script lang="ts">
-export default {
-  name: 'index'
-}
+  import Vue from 'vue'
+export default Vue.extend({
+  name: 'index' as string,
+
+  data: () =>  {
+    return {
+      loading: false as boolean
+    }
+  }
+})
 
 </script>
 
-<style>
-
+<style lang="scss">
+  img{
+    height: 250px;
+    margin: 50px;
+  }
 </style>
