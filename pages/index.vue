a668de0f5d8576cb6df4d55db5d9e36d3f0bad3d
B9a9s9 <bas.vaneck@ziggo.nl>
2020-02-16 21:49:23 +0100

    buttons are now (in) columns

--- pages/index.vue
+++ pages/index.vue
@@ -1,13 +1,20 @@
 <template>
   <section class="container has-text-centered">
-    <figure class="image is-400x400 is-inline-block has-margin-50">
+
+    <figure class="image is-256x256 is-inline-block has-margin-50">
       <img src="https://cdn.discordapp.com/attachments/648842340273750026/675728343584342039/Structure.png" alt="Logo">
     </figure>
 
-    <div class="container">
-      <b-button class="is-primary is-rounded is-large" icon-left="" tag="nuxt-link" to="/">Host</b-button>
-      <b-button class="is-rounded is-large" icon-left="" tag="nuxt-link" to="/">Join</b-button>
+    <div class="columns buttons is-centered">
+      <div class="column is-narrow is-narrow-mobile">
+        <b-button class="is-rounded is-large" icon-left="" tag="nuxt-link" to="/">Join</b-button>
+      </div>
+
+      <div class="column is-narrow is-narrow-mobile">
+        <b-button class="is-rounded is-large is-primary" icon-left="" tag="nuxt-link" to="/">Host</b-button>
+      </div>
     </div>
+
   </section>
 </template>
 
