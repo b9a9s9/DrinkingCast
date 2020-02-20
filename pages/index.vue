14d556de2bc41ba83585a2ac420c07b5c9aca93b
Bas <bas.vaneck@ziggo.nl>
2020-02-20 18:32:43 +0100

    containers as child of section

--- pages/index.vue
+++ pages/index.vue
@@ -1,31 +1,37 @@
 <template>
-  <section class="container has-text-centered">
+  <section class="section has-text-centered">
 
-    <figure class="image is-256x256 is-inline-block has-margin-50">
-      <img src="https://cdn.discordapp.com/attachments/648842340273750026/675728343584342039/Structure.png" alt="Logo">
-    </figure>
+    <div class="container">
+      <figure class="image is-256x256 is-inline-block has-margin-50">
+        <img src="https://cdn.discordapp.com/attachments/648842340273750026/675728343584342039/Structure.png"
+             alt="Logo">
+      </figure>
+    </div>
 
-    <div class="columns buttons is-centered">
-      <div class="column is-narrow is-narrow-mobile">
-        <b-button class="is-rounded is-large" icon-left="" tag="nuxt-link" to="/join_game">Join</b-button>
-      </div>
+    <div class="container">
+      <div class="columns buttons is-centered">
+        <div class="column is-narrow is-narrow-mobile">
+          <b-button class="is-rounded is-large" icon-left="" tag="nuxt-link" to="/join_game">Join</b-button>
+        </div>
 
-      <div class="column is-narrow is-narrow-mobile">
-        <b-button class="is-rounded is-large is-primary" icon-left="" tag="nuxt-link" to="/host_game">Host</b-button>
+        <div class="column is-narrow is-narrow-mobile">
+          <b-button class="is-rounded is-large is-primary" icon-left="" tag="nuxt-link" to="/host_game">Host</b-button>
+        </div>
       </div>
     </div>
+
   </section>
 </template>
 
 <script lang="ts">
-export default {
-  name: 'index' as string,
-}
+  export default {
+    name: 'index' as string,
+  };
 
 </script>
 
 <style lang="scss">
-  html{
+  html {
     background: #6b2510;
   }
 
