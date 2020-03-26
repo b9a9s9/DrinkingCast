f7eb8c73d40f34ee88c10c5c460fc247739f6615
dennisfrijlink <31823687+dennisfrijlink@users.noreply.github.com>
2020-03-26 18:25:49 +0100

    - Made first version of card component

---
+++ Client/components/Card.vue
@@ -0,0 +1,39 @@
+<template>
+    <li v-bind:style="{transform: 'rotate('+ rotate +'deg)'}"></li>
+</template>
+
+<script>
+export default {
+    name: 'Card',
+    props: {
+        rotate: Number
+    }
+}
+</script>
+
+<style scoped>
+li {
+    height: 170px;
+    width: 130px;
+    background: #51A375;
+    border: 8px solid white;
+    box-sizing: border-box;
+    left: 0; right: 0;
+    margin: auto;
+    position: absolute;
+    -webkit-border-radius: 4px;
+    -moz-border-radius: 4px;
+    border-radius: 4px;
+    -webkit-box-shadow: 0 1px 3px rgba(0,0,0,.24);
+    -moz-box-shadow: 0 1px 3px rgba(0,0,0,.24);
+    box-shadow: 0 1px 3px rgba(0,0,0,.24);
+    -webkit-box-sizing: border-box;
+    -moz-box-sizing: border-box;
+    -webkit-transform-origin: center 600px;
+    -moz-transform-origin: center 600px;
+    -o-transform-origin: center 600px;
+    -ms-transform-origin: center 600px;
+    transform-origin: center 350px;
+    transition: all 0.5s ease;
+}
+</style>
--- Client/pages/screen.vue
+++ Client/pages/screen.vue
@@ -1,24 +1,57 @@
 <template>
   <div>
     <h1>ID: {{ game_id }}</h1>
+    <div class="buttons">
+      <button @click="addCard">add</button>
+      <button @click="deleteCard">delete</button>
+    </div>
+    <p style="position: fixed; top: 30px; left: 0;">
+     {{ cards.length }}
+    </p>
+    <ul>
+      <transition-group name="item">
+        <Card v-for="card in cards" v-bind:key="card" :rotate="transforms[card]"></Card>
+      </transition-group>
+    </ul>
   </div>
 </template>
 
 <script>
 import io from "socket.io-client";
+import Card from '~/components/Card.vue';
 
 export default {
   name: 'screen',
   data() {
     return {
       game_id: null,
-      socket: null
+      socket: null,
+      cards: [0,1],
+      transforms: []
     }
+  },
+  components: {
+    Card
   },
   methods: {
     addClient() {
       this.game_id = Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15);
       this.socket.emit('addClient', { gameId: this.game_id });
+    },
+    getTransformPerCard(length) {
+      this.transforms = [];
+      for (var i = 0; i < length; i++) {
+        this.transforms.push((360/length) * (i + 1));
+      }
+    },
+    addCard() {
+      var lastCard = (this.cards.length - 1) + 1;
+      this.cards.push(lastCard);
+      this.getTransformPerCard(this.cards.length);
+    },
+    deleteCard() {
+      this.cards.splice(-1,1);
+      this.getTransformPerCard(this.cards.length);
     }
   },
   created() {
@@ -26,6 +59,7 @@ export default {
   },
   mounted() {
     this.addClient();
+    this.getTransformPerCard(this.cards.length);
   }
 }
 </script>
