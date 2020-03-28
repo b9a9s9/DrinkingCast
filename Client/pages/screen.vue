9fc12dd89f8c9485c127a557b6e96ed32a611f0c
Bas <bas.vaneck@ziggo.nl>
2020-03-28 22:12:57 +0100

    add typings

--- Client/pages/screen.vue
+++ Client/pages/screen.vue
@@ -16,40 +16,43 @@
   </div>
 </template>
 
-<script>
+<script lang="ts">
+import Vue from 'vue'
+
 import io from "socket.io-client";
 import Card from '~/components/Card.vue';
 
-export default {
-  name: 'screen',
+export default Vue.extend({
+  name: 'screen' as string,
   data() {
     return {
-      game_id: null,
-      socket: null,
-      cards: [0,1],
-      transforms: []
+      game_id: "" as string,
+      socket: null as any,
+      cards: [0,1] as number[],
+      transforms: [] as number[]
     }
   },
   components: {
     Card
   },
   methods: {
-    addClient() {
+    addClient(): void {
       this.game_id = Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15);
       this.socket.emit('addClient', { gameId: this.game_id });
     },
-    getTransformPerCard(length) {
+    getTransformPerCard(amountOfCards: number): void {
       this.transforms = [];
-      for (var i = 0; i < length; i++) {
-        this.transforms.push((360/length) * (i + 1));
+      for (let i = 0; i < amountOfCards; i++) {
+        // A list of transformations for cards (on index)
+        this.transforms.push((360/amountOfCards) * (i + 1));
       }
     },
-    addCard() {
-      var lastCard = (this.cards.length - 1) + 1;
+    addCard(): void {
+      let lastCard: number = (this.cards.length - 1) + 1;
       this.cards.push(lastCard);
       this.getTransformPerCard(this.cards.length);
     },
-    deleteCard() {
+    deleteCard(): void {
       this.cards.splice(-1,1);
       this.getTransformPerCard(this.cards.length);
     }
@@ -61,7 +64,7 @@ export default {
     this.addClient();
     this.getTransformPerCard(this.cards.length);
   }
-}
+})
 </script>
 
 <style scoped>
